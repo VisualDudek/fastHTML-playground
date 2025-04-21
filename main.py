7d7da7228b41caabc56496eb6a635f3f03ba306f
MarcinDudek <dudek.v@gmail.com>
2025-04-21 16:04:32 +0200

    turn off linters

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# type: ignore
+# ruff: noqa
 from fasthtml.common import *
 
 app, rt = fast_app(debug=True)
@@ -6,6 +8,11 @@ app, rt = fast_app(debug=True)
 @rt("/")
 def get():
     return Titled("FastHTML", P("Let's do this!"))
+
+
+@rt("/hello")
+def get():
+    return Titled("Hello, world!")
 
 
 serve()
