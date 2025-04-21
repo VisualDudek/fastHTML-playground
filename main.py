294c3457bf08f222a2e736e261dcced647deb2f6
MarcinDudek <dudek.v@gmail.com>
2025-04-21 16:40:31 +0200

    Refactor FastHTML application to include header configurations and enhance markdown rendering routes

--- main.py
+++ main.py
@@ -2,7 +2,15 @@
 # ruff: noqa
 from fasthtml.common import *
 
-app, rt = fast_app(debug=True)
+hdrs = (
+    MarkdownJS(),
+    HighlightJS(langs=["python", "javascript", "html", "css"]),
+)
+
+app, rt = fast_app(
+    debug=True,
+    hdrs=hdrs,
+)
 
 
 @rt("/")
@@ -13,6 +21,49 @@ def get():
 @rt("/hello")
 def get():
     return Titled("Hello, world!")
+
+
+@rt("/name/{name}")
+def get(name: str):
+    return Titled(f"Hello {name.title()}")
+
+
+content = """
+Here are some _markdown_ elements.
+
+- This is a list item
+- This is another list item
+- And this is a third list item
+
+**Fenced code blocks work here.**
+"""
+
+
+@rt("/markdown")
+def get(req):
+    return Titled("Markdown rendering example", Div(content, cls="marked"))
+
+
+code_example = """
+import datetime
+import time
+
+for i in range(10):
+    print(f"{datetime.datetime.now()}")
+    time.sleep(1)
+"""
+
+
+@rt("/code")
+def get(req):
+    return Titled(
+        "Markdown rendering example",
+        Div(
+            # The code example needs to be surrounded by
+            # Pre & Code elements
+            Pre(Code(code_example))
+        ),
+    )
 
 
 serve()
